54742b95bd795962955843b38afdd1700a46fb67
Phani Srikar <phanisrikar@Phanis-MacBook-Air-4.local>
2019-01-23 00:29:12 +0530

    AI movement and PLayeer movement are added , bug regarding missing AI moves will be tried to resolve in the next version

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -21,6 +21,76 @@ def printBoard(WhoseTurn, Board, AITurnCount):
 
     print ("Turn: " + str(WhoseTurn))
 
+    if WhoseTurn == "AI":
+        AITurnCount += 1
+        AImove(WhoseTurn, Board, AITurnCount)
+    if WhoseTurn == "PLAYER":
+        PlayerMove(WhoseTurn, Board, AITurnCount)
+
+
+def PlayerMove(WhoseTurn, Board, AITurnCount):
+    choice = input("Enter a number 0-8: ")
+    if choice.isdigit() == False:
+        print ("Keep it an integer, buddy.")
+        PlayerMove(WhoseTurn, Board, AITurnCount)
+    if int(choice) > 8 or int(choice) < 0:
+        print ("Make that a number between 0 and 8.")
+        PlayerMove(WhoseTurn, Board, AITurnCount)
+    if Board[int(choice)] == 'X' or Board[int(choice)] == 'O':
+        print ("That's already taken! Try again.")
+        PlayerMove(WhoseTurn, Board, AITurnCount)
+    else:
+        Board[int(choice)] = "O"
+    WhoseTurn = "AI"
+    WinnerCheck(WhoseTurn, Board, AITurnCount)
+
+
+
+def AImove(WhoseTurn, Board, AITurnCount):
+    alreadymoved = False
+    Pairs = ([0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6])
+
+
+
+    if AITurnCount == 1:
+        if Board[Random_Corner] != "O":
+            Board[Random_Corner] = "X"
+
+
+
+    else:
+        for x in Pairs:
+            if Board[x[0]] == "X" and Board[x[1]] == "X" and Board[x[2]] != "O":
+                Board[x[2]] = "X"
+                alreadymoved = True
+                break
+            if Board[x[1]] == "X" and Board[x[2]] == "X" and Board[x[0]] != "O":
+                Board[x[0]] = "X"
+                alreadymoved = True
+                break
+            if Board[x[0]] == "X" and Board[x[2]] == "X" and Board[x[1]] != "O":
+                Board[x[1]] = "X"
+                alreadymoved = True
+                break
+        for x in Pairs:
+            if alreadymoved == False:
+                if Board[x[0]] == "O" and Board[x[1]] == "O" and Board[x[2]] != "X":
+                    Board[x[2]] = "X"
+                    alreadymoved = True
+                    break
+                if Board[x[1]] == "O" and Board[x[2]] == "O" and Board[x[0]] != "X":
+                    Board[x[0]] = "X"
+                    alreadymoved = True
+                    break
+                if Board[x[0]] == "O" and Board[x[2]] == "O" and Board[x[1]] != "X":
+                    Board[x[1]] = "X"
+                    alreadymoved = True
+                    break
+
+    WhoseTurn = "PLAYER"
+    WinnerCheck(WhoseTurn, Board, AITurnCount)
+
+
 
 def WinnerCheck(WhoseTurn, Board, AITurnCount):
     for x in Pairs:
