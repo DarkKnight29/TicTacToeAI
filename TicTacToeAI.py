1490a78f312d55c56b34fedfaa064b0e7b63bb03
Phani Srikar <phanisrikar@Phanis-MacBook-Air-4.local>
2019-01-23 00:25:00 +0530

    Winner checking mechanism is added

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -21,8 +21,33 @@ def printBoard(WhoseTurn, Board, AITurnCount):
 
     print ("Turn: " + str(WhoseTurn))
 
-    
 
+def WinnerCheck(WhoseTurn, Board, AITurnCount):
+    for x in Pairs:
+        win1 = Board[x[0]]
+        win2 = Board[x[1]]
+        win3 = Board[x[2]]
+        if win1 == win2 and win2 == win3:
+            if win1 == "X":
+                print ("AI wins.")
+                end()
+            if win1 == "O":
+                print ("Human wins. Did you cheat?")
+                end()
+        else:
+            filledspaces = 0
+            for i in range(8):
+                if Board[i] != " " and Board[i] != "_":
+                    filledspaces += 1
+                if filledspaces == 8:
+                    print ("A draw! You will never win!")
+                    end()
 
+    printBoard(WhoseTurn, Board, AITurnCount)
 
-printBoard(WhoseTurn, Board, AITurnCount)
+def end():
+    print ("Here is the final Board.")
+    print (Board[0] + '{}'.format('|') + Board[1] + '{}'.format('|') + Board[2])
+    print (Board[3] + '{}'.format('|') + Board[4] + '{}'.format('|') + Board[5])
+    print (Board[6] + '{}'.format('|') + Board[7] + '{}'.format('|') + Board[8])
+    sys.exit(0)
